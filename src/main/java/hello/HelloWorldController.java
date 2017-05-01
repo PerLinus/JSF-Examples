374f14a1ec24a697c320bcea714b65b39060e71c
Björn Eiderbäck <bjorn.eiderback@gmail.com>
2017-05-01 11:38:05 +0200

    Navigeringsexempel. Vi frågar bönan/kontrollen

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -10,4 +10,10 @@ public class HelloWorldController {
     public String getMessage() {
         return "Hello World!";
     }
+
+    //När vi vill navigera kan vi göra detta via kontrollern
+    public String goToPage() {
+        //Nu säger vi för enkhelts skull att vi ska till startsidsan
+        return "index";
+    }
 }
